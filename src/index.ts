ae3bb839fad282b564b20c0f9e33ff7131a99b57
Adel Ghaenian <adelghaeinian@gmail.com>
2023-11-22 11:01:14 -0400

    webhook for firebase :)
    this webhook allows users to signup with google/apple and then firebase sends a request to this webhook to complete the signup process and create an entity on our database for that user.

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import * as functions from "firebase-functions";
+import axios from "axios";
+
+export const sendUserToServer = functions.auth.user().onCreate(async (user) => {
+  // Retrieve your sync secret from Firebase configuration
+  const syncSecret = functions.config().fsync.secret;
+
+  // User data that Firebase provides
+  const userData = {
+    uid: user.uid,
+    email: user.email,
+    displayName: user.displayName,
+    photoURL: user.photoURL,
+  };
+
+  const IURL = "https://www.phantomphood.ai";
+  const serverEndpoint = IURL + "/api/v1/auth/firebaseSync";
+
+  try {
+    // Send a POST request to your server with the secret in the Authorization header
+    const response = await axios.post(serverEndpoint, userData, {
+      headers: {Authorization: syncSecret},
+    });
+    console.log("Successfully sent user data to server:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error sending user data to server:", error);
+    throw new functions.https.HttpsError(
+      "internal",
+      "Unable to send user data"
+    );
+  }
+});
